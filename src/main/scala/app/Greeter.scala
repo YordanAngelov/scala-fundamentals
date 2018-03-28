5556b2091f27bb54c8efe118867ccb78d00a3258
Yordan Angelov <yordan.angelov@ten10.com>
2018-03-28 13:51:15 +0100

    second commit

--- src/main/scala/app/Greeter.scala
+++ src/main/scala/app/Greeter.scala
@@ -1,7 +1,13 @@
 package app
 
+import scala.io.StdIn
+
 object Greeter extends App {
 
-  println(s"Hello !")
+  def greet(n: String): Unit = println(s"Hello $n!")
+
+  val name = StdIn.readLine("What is your name? ")
+
+  greet(name)
 
 }
