3c2a509b9128ebd2420a450664260881f78cb154
Yordan Angelov <yordan.angelov@ten10.com>
2018-04-03 16:34:46 +0100

    pet repo wip

--- src/main/scala/app/models/Pet.scala
+++ src/main/scala/app/models/Pet.scala
@@ -24,8 +24,5 @@ final class Dog extends Pet{
   override val name: String = "Dog"
   override val speak: String = "Woof!"
 
-//  The below contains all of the subtypes of Pet, e.g. all of the classes or objects that extend it
-  private val _pets: ArrayBuffer[Pet] = new ArrayBuffer[Pet]()
-
 }
 
