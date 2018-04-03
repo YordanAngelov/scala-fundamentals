0b321b3dc0ee7d5792e80f149cdc84e9d8410527
Yordan Angelov <yordan.angelov@ten10.com>
2018-04-03 16:25:46 +0100

    added trait pet and two classes, cat and dog

--- src/main/scala/app/models/Pet.scala
+++ src/main/scala/app/models/Pet.scala
@@ -1,5 +1,31 @@
 package app.models
 
-trait Pet {
+import scala.collection.mutable.ArrayBuffer
+
+//  A sealed trait can only be extended in the same file
+sealed trait Pet {
+
+  val name: String
+  def speak: String
+
+//  Traits can have partial implementation
 
 }
+
+final class Cat extends Pet{
+
+  override val name: String = "Cat"
+  override def speak: String = "Meow!"
+
+}
+
+final class Dog extends Pet{
+
+  override val name: String = "Dog"
+  override val speak: String = "Woof!"
+
+//  The below contains all of the subtypes of Pet, e.g. all of the classes or objects that extend it
+  private val _pets: ArrayBuffer[Pet] = new ArrayBuffer[Pet]()
+
+}
+
